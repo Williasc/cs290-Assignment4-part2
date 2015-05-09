2b8917ecccf6e14e5a5a3bf5061ca39a2bec218e
Scott Williams <williasc@onid.oregonstate.edu>
2015-05-09 17:37:19 +0000

    initial commit to add log.txt and videoStore.php

---
+++ src/videoStore.php
@@ -0,0 +1,13 @@
+<?php
+ini_set('display_errors', 'On');
+include 'storedInfo.php';
+
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "williasc-db", $mypassord, "williasc-db");
+if ($mysqli->connect_errno){
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+else{
+    echo "Connection worked!<br>";
+}
+
+?>
